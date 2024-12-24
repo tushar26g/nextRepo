86521fec909d17dfdc0f1a5eaee297459643622d
Kushagra Agrawal <kushjanhvi@gmail.com>
2024-12-24 21:37:48 +0530

    homepage, layout, axios

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import "katex/dist/katex.min.css";
+export default function Home() {
+  return (
+    <div className="font-sans">
+      <h1 className="text-3xl font-bold mb-6">Welcome</h1>
+      {/* Your homepage content */}
+    </div>
+  );
+}
