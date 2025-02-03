41be0f870b1572023cfe546e8ec822884b9bdb58
tushar26g <tushar26g@gmail.com>
2025-02-03 22:48:27 +0530

    remove SideBar from Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { AdPanel } from "./AdPanel";
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
-import { Sidebar } from "./Sidebar";
+// import { Sidebar } from "./Sidebar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -12,7 +12,7 @@ export function Layout({ children }: LayoutProps) {
     <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
       <Navbar />
       <div className="flex-1 flex pt-16">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <main className="flex-1 mx-64 p-6">{children}</main>
         <AdPanel />
       </div>
